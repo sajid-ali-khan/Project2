7e7a440f13ed622322dd44ee2bc9bc9c6164089f
sajid@windows <sajid@windows>
2025-06-16 12:00:37 +0530

    Dashboard page completed

---
+++ Project2/Models/ClassModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2.Models
+{
+    public class ClassModel{
+        public string SubjectName { get; set; }
+        public string YearAndSection { get; set; }
+        public string StartTime { get; set; }
+        public string ClassLocation { get; set; }
+        public string Status { get; set; }
+        public Color BgColor { get; set; }
+        public Color TColor { get; set; }
+
+        public ClassModel(string sname, string yas, string st, string cl, ClassStatus status)
+        {
+            SubjectName = sname;
+            YearAndSection = yas;
+            StartTime = st;
+            ClassLocation = cl;
+            Status = Enum.GetName(typeof(ClassStatus), status);
+            switch (status)
+            {
+                case ClassStatus.upcoming:
+                    BgColor = Color.FromHex("#ffedd5");
+                    TColor = Color.FromHex("#9a3412");
+                    break;
+                case ClassStatus.completed:
+                    BgColor = Color.FromHex("#dcfce7");
+                    TColor = Color.FromHex("#166543");
+                    break;
+                case ClassStatus.ongoing:
+                    BgColor = Color.FromHex("#dbeafe");
+                    TColor = Color.FromHex("#1e40af");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(status), status, null);
+            }
+        }
+    }
+
+    public enum ClassStatus
+    {
+        upcoming,
+        ongoing,
+        completed
+    }
+}
---
+++ Project2/VIewModels/ClassViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project2.Models;
+
+namespace Project2.VIewModels
+{
+    class ClassViewModel
+    {
+        public ICollection<ClassModel> Classes { get; set; }
+        public ClassViewModel()
+        {
+            Classes =
+            [
+                new ClassModel("Computer Science", "3rd Year - A", DateTime.Now.AddHours(1).ToString("t"), "CS-101", ClassStatus.upcoming),
+                new ClassModel("Data Structures", "2nd Year - B", DateTime.Now.AddHours(2).ToString("t"), "CS-102", ClassStatus.completed),
+                new ClassModel("Web Development", "3rd Year - A", DateTime.Now.AddHours(3).ToString("t"), "CS-103", ClassStatus.ongoing),
+                new ClassModel("Database Systems", "4th Year - A", DateTime.Now.AddHours(4).ToString("t"), "CS-104", ClassStatus.upcoming),
+            ];
+        }
+    }
+}
